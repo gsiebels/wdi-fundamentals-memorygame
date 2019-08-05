6e1255308ea7fbf2be77e28e3777ba5d3d1d1f91
Gerardo Siebels <gerardo@Gerardos-iMac.local>
2019-08-05 17:15:08 +0100

    created game logic

--- Memory-Game/js/main.js
+++ Memory-Game/js/main.js
@@ -1,9 +1,33 @@
-console.log("Up and running!");
+//Memory Game
+//Game target get same card after first pick
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 
-console.log("User Flipped " + cardOne);
-console.log("User Flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+
+
+cardsInPlay.push(cardOne);
+
+
+console.log("User flipped a queen");
+
+var cardTwo = cards[2];
+
+console.log("User flipped a king")
+
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry!, try again");
+	}
+
+}
+
+
+
+
+
