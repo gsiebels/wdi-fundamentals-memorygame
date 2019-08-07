a3bddf3865bae0c67eada45272a4ba0d353d3454
Gerardo Siebels <gerardo@Gerardos-iMac.local>
2019-08-07 12:32:32 +0100

    created game logic

--- Memory-Game/js/main.js
+++ Memory-Game/js/main.js
@@ -4,28 +4,31 @@
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
 
 
-cardsInPlay.push(cardOne);
-
-
-console.log("User flipped a queen");
-
-var cardTwo = cards[2];
-
-console.log("User flipped a king")
-
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
+var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+		console.log("You found a match!");
 	} else {
-		alert("Sorry!, try again");
+		console.log("Sorry!, try again");
 	}
 
-}
+};
+
+
+var flipCard = function(cardId){
+	console.log("User flipped" + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId])
+	checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
+
+
+
+
 
 
 
