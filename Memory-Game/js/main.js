fd49be9b12292aeb07e424bbc4bfbc162de0060e
Gerardo Siebels <gerardo@Gerardos-iMac.local>
2019-08-08 10:44:48 +0100

    added objects

--- Memory-Game/js/main.js
+++ Memory-Game/js/main.js
@@ -2,25 +2,47 @@
 //Game target get same card after first pick
 
 
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
+var cards = [
+	{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "image/queen-of-hearts.png"
+	},
+	{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "image/queen-of-diamonds.png"
+	},
+	{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "image/king-of-hearts.png"
+	},
+	{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "image/king-of-diamonds.png"
+	},
+]
 
+var cardsInPlay = [];
 
 var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-		console.log("Sorry!, try again");
+		alert("Sorry!, try again");
 	}
 
 };
 
 
 var flipCard = function(cardId){
-	console.log("User flipped" + cards[cardId]);
-
-	cardsInPlay.push(cards[cardId])
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank)
 	checkForMatch();
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 };
 
 flipCard(0);
